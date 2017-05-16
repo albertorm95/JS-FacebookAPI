6b27e1846243bb2a4019e790f716c6121484af47
Alberto Rojas <albertorojasm95@gmail.com>
2017-05-16 08:52:29 -0500

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+// Dependencies
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+var livereload = require('gulp-livereload');
+ 
+// Task
+gulp.task('default', function() {
+	// listen for changes
+	livereload.listen();
+	// configure nodemon
+	nodemon({
+		// the script to run the app
+		script: 'app.js',
+		ext: 'js html css'
+	}).on('restart', function(){
+		// when the app has restarted, run livereload.
+		gulp.src('app.js')
+			.pipe(livereload())
+	})
+})
